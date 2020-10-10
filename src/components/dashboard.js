a8e24c04dca309a8e2245f99eb8b4e67b9f6218e
Muhammad Waqar <mohammadwaqar401@gmail.com>
2020-10-10 16:23:00 +0500

    added dropdown

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,6 +3,8 @@ import './dashboard.css';
 import WidgetBar from './widgetBar';
 import WidgetDoughnut from './widgetDoughnut';
 import WidgetText from './widgetText';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 
 const config = {
@@ -17,13 +19,46 @@ class Dashboard extends Component {
     
     constructor(){
         super();
+        this.state ={
+            items: [],
+            dropdownOptions: [],
+            selectedValue: null,
+        }
     }
 
+    getData = arg =>{
+
+    }
+
+    updateDashboard(){
+
+    }
     componentDidMount(){
         fetch(url)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+           let batchRowValue = data.valueRanges[0].values;
+           const rows = [];
+           for(let i=1; i < batchRowValue.length; i++){
+                let rowObject = {};
+                for(let j=0; j<batchRowValue[i].length; j++){
+                   rowObject[batchRowValue[0][j]] = batchRowValue[i][j];
+                   rows.push(rowObject);
+                }
+
+                let dropdownOptions = [];
+                for(let i=0; i<rows.length; i++){
+                   dropdownOptions.push(rows[i].month);
+                }
+
+                dropdownOptions = Array.from(new Set(dropdownOptions)).reverse();
+                this.setState({
+                    items: rows,
+                    dropdownOptions: dropdownOptions,
+                    selectedValue: "Jan 2018"
+                }, () => this.getData("Jan 2018"));
+
+           } 
         });
     }
 
@@ -52,8 +87,16 @@ class Dashboard extends Component {
             }
             
         ];
+
+        const options = [
+            'one', 'two', 'three'
+          ];
+
+        const defaultOption = options[0];
+
         return (
          <div>
+             <Dropdown options={this.state.dropdownOptions} onChange={this.updateDashboard} value={this.state.selectedValue} placeholder="Select an option" />;
              <WidgetText title="Title" value="190" descr="Very good" />
              <WidgetBar title="WidgetBar" data={chartData}/>
              <WidgetDoughnut title="widgetDoughnut" data={chartData}/>
